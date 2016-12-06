15c074c4c34144c5f9609380d5f09aac96cbc382
[username] <zhubd@yaocaimaimai.com>
2016-12-06 15:12:45 +0800

    fix bug

--- app/src/router.js
+++ app/src/router.js
@@ -15,69 +15,23 @@ import transaction from './views/transaction'
 import search from './views/search'
 import lowPriceRes from './views/lowPriceRes'
 import urgentNeed from './views/urgentNeed'
-<<<<<<< HEAD
 import myAttention from './views/myAttention'
 import myResource from './views/myResource'
 import register from './views/register'
 import findPassWord from './views/findPassWord'
 import mySet from './views/mySet'
 import revisePassWord from './views/revisePassWord'
-import revisePassWordConfirm  from './views/revisePassWordConfirm'
+import revisePassWordConfirm from './views/revisePassWordConfirm'
 import addressManage from './views/addressManage'
 import addressRevise from './views/addressRevise'
 import feedBack from './views/feedBack'
 import myOrder from './views/myOrder'
-
-
-
-module.exports = {
-  mode: 'hash',
-  base: __dirname,
-  routes: [
-    { name: 'login', path: '/login', component: login },
-    { name: 'main',
-      path: '/main',
-      component: main,
-      children: [
-          { name: 'hello', path: '/hello', component: hello },
-          { name: 'menu', path: '/menu', component: menu }
-      ]
-    },
-    { name: 'home', path: '/home', component: home },
-    { name: 'drugResTable', path: '/drugResTable', component: drugResTable },
-    { name: 'marketQuotation', path: '/marketQuotation', component: marketQuotation },
-    { name: 'mySupply', path: '/mySupply', component: mySupply },
-    { name: 'myDemond', path: '/myDemond', component: myDemond },
-    { name: 'provenanceSelection', path: '/provenanceSelection', component: provenanceSelection },
-    { name: 'accountInfo', path: '/accountInfo', component: accountInfo },
-    { name: 'accountInfoFinish', path: '/accountInfoFinish', component: accountInfoFinish },
-    { name: 'drugResTableDetail', path: '/drugResTableDetail/:drugId', component: drugResTableDetail },
-    { name: 'transaction', path: '/transaction', component: transaction },
-    { name: 'search', path: '/search', component: search },
-    { name: 'lowPriceRes', path: '/lowPriceRes', component: lowPriceRes },
-    { name: 'urgentNeed', path: '/urgentNeed', component: urgentNeed },
-    { name: 'myAttention', path: '/myAttention', component: myAttention },
-    { name: 'myResource', path: '/myResource', component: myResource },
-    { name: 'register', path: '/register', component: register },
-    { name: 'findPassWord', path: '/findPassWord', component: findPassWord },
-    { name: 'mySet', path: '/mySet', component: mySet },
-    { name: 'revisePassWord', path: '/revisePassWord', component: revisePassWord },
-    { name: 'revisePassWordConfirm', path: '/revisePassWordConfirm', component: revisePassWordConfirm },
-    { name: 'addressManage', path: '/addressManage', component: addressManage },
-    { name: 'addressRevise', path: '/addressRevise', component: addressRevise },
-    { name: 'feedBack', path: '/feedBack', component: feedBack },
-    { name: 'myOrder', path: '/myOrder', component: myOrder },
-    { path: '*', redirect: '/home' }
-  ]
-=======
-import payMoney from './views/payMoney'
 import needDetail from './views/needDetail'
 import resourceDetail from './views/resourceDetail'
-import myAttention from './views/myAttention'
-import myResource from './views/myResource'
-import register from './views/register'
 import needReleaseSuccess from './views/needReleaseSuccess'
 import supplyReleaseSuccess from './views/supplyReleaseSuccess'
+
+
 
 module.exports = {
     mode: 'hash',
@@ -105,15 +59,21 @@ module.exports = {
         { name: 'search', path: '/search', component: search },
         { name: 'lowPriceRes', path: '/lowPriceRes', component: lowPriceRes },
         { name: 'urgentNeed', path: '/urgentNeed', component: urgentNeed },
-        { name: 'payMoney', path: '/payMoney', component: payMoney },
-        { name: 'needDetail', path: '/needDetail/:needId', component: needDetail },
-        { name: 'resourceDetail', path: '/resourceDetail/:sourceId', component: resourceDetail },
         { name: 'myAttention', path: '/myAttention', component: myAttention },
         { name: 'myResource', path: '/myResource', component: myResource },
         { name: 'register', path: '/register', component: register },
+        { name: 'findPassWord', path: '/findPassWord', component: findPassWord },
+        { name: 'mySet', path: '/mySet', component: mySet },
+        { name: 'revisePassWord', path: '/revisePassWord', component: revisePassWord },
+        { name: 'revisePassWordConfirm', path: '/revisePassWordConfirm', component: revisePassWordConfirm },
+        { name: 'addressManage', path: '/addressManage', component: addressManage },
+        { name: 'addressRevise', path: '/addressRevise', component: addressRevise },
+        { name: 'feedBack', path: '/feedBack', component: feedBack },
+        { name: 'myOrder', path: '/myOrder', component: myOrder },
         { name: 'needReleaseSuccess', path: '/needReleaseSuccess', component: needReleaseSuccess },
         { name: 'supplyReleaseSuccess', path: '/supplyReleaseSuccess', component: supplyReleaseSuccess },
+        { name: 'needDetail', path: '/needDetail/:needId', component: needDetail },
+        { name: 'resourceDetail', path: '/resourceDetail/:sourceId', component: resourceDetail },
         { path: '*', redirect: '/home' }
     ]
->>>>>>> ae1d5b00e4edfeb8ea58bcb61f8bf1ba3aaa14f3
 }
