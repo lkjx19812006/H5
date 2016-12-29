e75b1b25c0b7c6c9c4e1ac297f198bd30cc1849c
wxb <317505615@qq.com>
2016-12-29 10:54:08 +0800

    fix 地址选择器

--- app/src/views/addressRevise.vue
+++ app/src/views/addressRevise.vue
@@ -45,9 +45,9 @@
 <script>
 import common from '../common/common.js'
 import httpService from '../common/httpService.js'
-import areaJson from '../common/data.json'
+import areaJson from '../common/areaData'
 
- const addressArr = ['北京','广东','上海','天津','重庆','辽宁','江苏','湖北','四川','陕西','河北','山西','河南','吉林','黑龙江','内蒙古','山东','安徽','浙江','福建','湖南','广西','江西','贵州','云南','西藏','海南','甘肃','宁夏','青海','新疆','香港','澳门','台湾'];
+const addressArr = ['北京市', '天津市', '河北省', '山西省', '内蒙古自治区', '辽宁省', '吉林省', '黑龙江省', '上海市', '江苏省', '浙江省', '安徽省', '福建省', '江西省', '山东省', '河南省', '湖北省', '湖南省', '广东省', '广西壮族自治区', '海南省', '重庆市', '四川省', '贵州省', '云南省', '西藏自治区', '陕西省', '甘肃省', '青海省', '宁夏回族自治区', '新疆维吾尔族自治区'];
 
 
 export default {
@@ -63,6 +63,11 @@ export default {
                     addressDistrict: '',
                     detailAddr: ''
                 },
+                areaParam: {
+                    addressProvince: '北京市',
+                    addressCity: '北京市',
+                    addressDistrict: '东城区'
+                },
                 addressSlots: [{
                     flex: 1,
                     values: addressArr,
@@ -74,7 +79,7 @@ export default {
                     className: 'slot2'
                 }, {
                     flex: 1,
-                    values: ['北京'],
+                    values: ['北京市'],
                     className: 'slot3',
                     textAlign: 'center'
                 }, {
@@ -83,7 +88,7 @@ export default {
                     className: 'slot4'
                 }, {
                     flex: 1,
-                    values: ['东城区', '西城区','朝阳区', '丰台区', '石景山区', '海淀区', '门头沟区', '房山区', '通州区', '顺义区', '昌平区', '大兴区', '怀柔区', '平谷区', '密云区', '延庆区'],
+                    values: ['东城区', '西城区', '朝阳区', '丰台区', '石景山区', '海淀区', '门头沟区', '房山区', '通州区', '顺义区', '昌平区', '大兴区', '怀柔区', '平谷区', '密云区', '延庆区'],
                     className: 'slot5',
                     textAlign: 'center'
                 }],
@@ -121,10 +126,43 @@ export default {
                 })
             },
             onAddressChange(picker, values) {
-              console.log(areaJson.data);
-                this.obj.addressProvince = values[0];
-                this.obj.addressCity = values[1];
-                this.obj.addressDistrict = values[2];
+                let cityArr = [];
+                let districtArr = [];
+                let provinceId;
+                let cityId = '';
+                if (this.obj.addressProvince != values[0]) {
+                    for (var i = 0; i < areaJson.province.length; i++) {
+                        if (values[0] == areaJson.province[i].value) {
+                            provinceId = areaJson.province[i].id;
+                        }
+                    }
+                    for (var i = 0; i < areaJson.city.length; i++) {
+                        if (areaJson.city[i].parentId == provinceId) {
+                            if (!cityId) cityId = areaJson.city[i].id;
+                            cityArr.push(areaJson.city[i].value);
+                        }
+                    }
+                } else if (this.obj.addressCity != values[1]) {
+                    for (var i = 0; i < areaJson.city.length; i++) {
+                        if (areaJson.city[i].value == values[1]) {
+                            cityId = areaJson.city[i].id;
+                        }
+                    }
+
+                }
+                if (cityId) {
+                    for (var i = 0; i < areaJson.county.length; i++) {
+                        if (areaJson.county[i].parentId == cityId) {
+                            districtArr.push(areaJson.county[i].value);
+                        }
+                    }
+                }
+                if (cityArr.length > 0) picker.setSlotValues(1, cityArr);
+                if (districtArr.length > 0) picker.setSlotValues(2, districtArr);
+                this.areaParam.addressProvince = values[0];
+                this.areaParam.addressCity = values[1];
+                this.areaParam.addressDistrict = values[2];
+               
             },
             selectPlace() {
                 this.show = true;
@@ -133,6 +171,9 @@ export default {
                 this.show = false;
             },
             confirmIt() {
+                this.obj.addressProvince = this.areaParam.addressProvince;
+                this.obj.addressCity = this.areaParam.addressCity;
+                this.obj.addressDistrict = this.areaParam.addressDistrict;
                 this.show = false;
             },
             confirm() {
