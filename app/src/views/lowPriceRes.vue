dbd312b06ea58a367595a257d42fb62a751e85cd
wxb <317505615@qq.com>
2017-01-09 16:08:11 +0800

    fix 404页面

--- app/src/views/lowPriceRes.vue
+++ app/src/views/lowPriceRes.vue
@@ -8,7 +8,7 @@
                     <mt-loadmore :top-method="loadTop" @top-status-change="handleTopChange" :bottom-method="loadBottom" @bottom-status-change="handleBottomChange" :bottom-all-loaded="allLoaded" ref="loadmore">
                     <ul class="page-loadmore-list">
                         <li v-for="(todo,index) in todos" class="page-loadmore-listitem list_content_item" @click="jumpDetail(todo.id)">
-                            <img v-lazy="todo.image[0]" class="list_images">
+                            <img v-bind:src="todo.image[0]" class="list_images">
                             <div class="res_content">
                                 <div class="res_content_center">
                                     <div><img src="/static/images/bao.png"><img src="/static/images/zheng.png">{{todo.breedName}}</div>
