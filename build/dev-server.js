a54508c7b41f9ef095c5ff5912c537ef8f789d93
yinqian <yinq@yaocaimaimai.com>
2017-09-04 09:06:11 +0800

    “H5提交代码

--- build/dev-server.js
+++ build/dev-server.js
@@ -29,7 +29,7 @@ var hotMiddleware = require('webpack-hot-middleware')(compiler)
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
+    hotMiddleware.publish({action: 'reload'})
     cb()
   })
 })
@@ -38,7 +38,7 @@ compiler.plugin('compilation', function (compilation) {
 Object.keys(proxyTable).forEach(function (context) {
   var options = proxyTable[context]
   if (typeof options === 'string') {
-    options = { target: options }
+    options = {target: options}
   }
   app.use(proxyMiddleware(context, options))
 })
@@ -57,6 +57,8 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
+
+app.listen(port, '192.168.1.56')
 module.exports = app.listen(port, function (err) {
   if (err) {
     console.log(err)
